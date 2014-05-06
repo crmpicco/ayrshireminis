727e5b3e34e41987207d76abedfd0ea880b9fd00
Craig R Morton <cmorton@reiss.co.uk>
2014-05-06 17:59:50 +0100

    Beginning to integrate the Sonata admin project into the site

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,10 +19,14 @@ class AppKernel extends Kernel
             new Acme\HelloBundle\AcmeHelloBundle(),
             new AyrshireMinis\ContactBundle\AyrshireMinisContactBundle(),
             new AyrshireMinis\CommonBundle\AyrshireMinisCommonBundle(),
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            //$bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
