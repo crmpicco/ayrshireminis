96f58b52b77bd9d2fcf6bcd9229c90e1b0c9d4d1
Craig R Morton <cmorton@reiss.co.uk>
2014-06-24 18:03:21 +0100

    Adding An *Add* button to the Gallery page and adding the GalleryImageType into the Controller

--- src/AyrshireMinis/GalleryBundle/Controller/DefaultController.php
+++ src/AyrshireMinis/GalleryBundle/Controller/DefaultController.php
@@ -2,7 +2,15 @@
 
 namespace AyrshireMinis\GalleryBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Template,
+    Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+use Symfony\Component\HttpFoundation\Session\Session;
+
+use AyrshireMinis\GalleryBundle\Form\GalleryImageType,
+    AyrshireMinis\GalleryBundle\Entity\GalleryImage;
 
 class DefaultController extends Controller
 {
@@ -11,8 +19,19 @@ class DefaultController extends Controller
         return $this->render('AyrshireMinisGalleryBundle:Default:index.html.twig');
     }
 
+    /**
+     * @Template("AyrshireMinisGalleryBundle:Default:add.html.twig")
+     */
     public function addAction()
     {
-        return $this->render('AyrshireMinisGalleryBundle:Default:index.html.twig');
+        //use the createForm method to get a symfony form instance of our form
+        $form = $this->createForm(new GalleryImageType());
+
+        return array(
+            //pass the form to our template, must be a form view using ->createView()
+            'form' => $form->createView()
+        );
+
+        //return $this->render('AyrshireMinisGalleryBundle:Default:add.html.twig');
     }
 }
