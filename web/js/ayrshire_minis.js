4ff253c87199f8ad802e0bde61c3116d7d59aa33
Craig R Morton <cmorton@reiss.co.uk>
2014-12-10 13:43:40 +0000

    Adding Google Maps to the right hand column of the Contact page

--- web/js/ayrshire_minis.js
+++ web/js/ayrshire_minis.js
@@ -1,7 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    //$('.link_go').click(function() {
-    //    window.open($(this).attr('data-url'), '_blank');
-    //});
+    // Google Maps on the /contact page
+    function init_map() {
+        var var_location = new google.maps.LatLng(55.518282, -4.379168);
+
+        var var_mapoptions = {
+            center: var_location,
+            zoom: 14
+        };
+
+        var var_marker = new google.maps.Marker({
+            position: var_location,
+            map: var_map,
+            title: "Venice"
+        });
+
+        var var_map = new google.maps.Map(document.getElementById("map-container"),
+            var_mapoptions);
+
+        var_marker.setMap(var_map);
+
+    }
+
+    google.maps.event.addDomListener(window, 'load', init_map);
 
 });
