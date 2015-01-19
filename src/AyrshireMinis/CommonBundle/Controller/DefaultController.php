2ff1bc099b943740776291a07632e86f074bd20a
Craig R Morton <cmorton@reiss.co.uk>
2015-01-19 17:03:45 +0000

    Converting more routing to YAML from Annotations
    Handling legacy PHPBB2 forum URL routes (temporarily)

--- src/AyrshireMinis/CommonBundle/Controller/DefaultController.php
+++ src/AyrshireMinis/CommonBundle/Controller/DefaultController.php
@@ -1,44 +1,63 @@
 <?php
 
+/**
+ * Common bundle controller to handle homepage, about page and links page
+ *
+ * @author Craig R Morton <crmpicco@aol.co.uk>
+ * @date   19-Jan-2015
+ */
+
 namespace AyrshireMinis\CommonBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller,
-    Sensio\Bundle\FrameworkExtraBundle\Configuration\Route,
-    Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
-     * @Template()
+     * Homepage
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function indexAction()
     {
-        return array();
+        // catch legacy newsletter unsubscribe requests and redirect them to the homepage for the time being
+        if (strpos($_SERVER['REQUEST_URI'], 'newsunsub.php')) {
+            return $this->redirect($this->generateUrl('ayrshireminis_common_default_index'), 301);
+        }
+
+        return $this->render('AyrshireMinisCommonBundle:Default:index.html.twig');
     }
 
     /**
-     * @Route("/about", name="ayrshireminis_about")
-     * @Template()
+     * About page
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function aboutAction()
     {
-        //use the createForm method to get a symfony form instance of our form
-//        $form = $this->createForm(new ContactType());
+        // catch legacy forum (PHPBB2) requests and redirect them to the about page for the time being
+        if (strpos($_SERVER['REQUEST_URI'], 'phpBB2')) {
+            return $this->redirect($this->generateUrl('ayrshireminis_about'), 301);
+        }
 
-        return array(
-            //pass the form to our template, must be a form view using ->createView()
-//            'form' => $form->createView()
-        );
+        return $this->render('AyrshireMinisCommonBundle:Default:about.html.twig');
     }
 
     /**
-     * @Route("/links", name="ayrshireminis_links")
-     * @Template()
+     * Get all the links and display them on the /links page
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function linksAction()
     {
+        // catch legacy links.php requests and redirect them to the new links page at /links
+        if (strpos($_SERVER['REQUEST_URI'], 'links.php')) {
+            return $this->redirect($this->generateUrl('ayrshireminis_links'), 301);
+        }
+
         $links = $this->getDoctrine()->getRepository('AyrshireMinisCommonBundle:Link')->findAll();
+
         return $this->render('AyrshireMinisCommonBundle:Default:links.html.twig', array('links' => $links));
     }
 }
